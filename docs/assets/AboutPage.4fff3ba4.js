09ff812adc13be991fadd43ee03582bb0794ade7
Maria Walker <mariawalker3232@gmail.com>
2022-05-17 17:47:05 -0600

    test

---
+++ docs/assets/AboutPage.4fff3ba4.js
@@ -0,0 +1,1 @@
+import{_ as e}from"./index.b1e76cfc.js";import{d as o,e as t,f as s}from"./vendor.0c60887e.js";const a={name:"AboutPage"},c={class:"about"},n=s("h1",null,"This is the about page",-1),r=[n];function _(d,i,p,f,u,h){return o(),t("div",c,r)}var b=e(a,[["render",_]]);export{b as default};
